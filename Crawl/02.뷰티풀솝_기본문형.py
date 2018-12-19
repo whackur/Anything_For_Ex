da83ce2ad3f8433dce80763837dfe805c82b26ae
JaeWoong Gong <ioumelon7@gmail.com>
2018-12-19 14:10:28 +0000

    Upload New File

---
+++ Crawl/02.뷰티풀솝_기본문형.py
@@ -0,0 +1,9 @@
+#pip install bs4
+import bs4
+
+html_str = "<html><head>This is Head</head><div>Something In Here</div/></html>"
+bsObj = bs4.BeautifulSoup(html_str, "html.parser")
+
+print(type(bsObj))
+print(bsObj)
+print(bsObj.find("div"))
